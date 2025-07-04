488b731e3ffae8cf9342c3f2b35f4957b7015557
Dmitry <110281080+poitrew11@users.noreply.github.com>
2025-07-04 18:28:38 +0300

    Update main.py

--- Multi_Agent_RAG_system/main.py
+++ Multi_Agent_RAG_system/main.py
@@ -1,1 +1,8 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv(dotenv_path = ".env", override = True)
+
+llm = ChatOpenAI(model = "meta-llama/Llama-3.3-70B-Instruct", temperature = 0)
+
 
